d544ef271f0b041ccbdc8e379564f11e2308165b
KetchuppOfficial <dolgov04@list.ru>
2022-05-12 13:07:08 +0300

    More correct version of the program. The location of 'charging time' and 'discharging time' labels is calculated automatically

--- Python_Scripts/8-graphs.py
+++ Python_Scripts/8-graphs.py
@@ -4,14 +4,9 @@ import numpy as np
 V_max = 3.3
 levels = 2**8
 
-def Get_Step ():
-    with open ('settings.txt', 'r') as settings:
-        tmp = [float(num) for num in settings.read().split("\n")]
-    return tmp[1]
-
-def ADC_To_Voltage ():
+def ADC_To_Voltage (volt_step):
     data_arr = np.loadtxt ('data.txt', dtype = int)
-    volt_arr = data_arr / levels * V_max
+    volt_arr = data_arr * volt_step
 
     return volt_arr
 
@@ -21,9 +16,13 @@ def Time_Division (volt_arr, time_step):
     
     return time_arr
 
-volt_arr = ADC_To_Voltage ()
+with open ('settings.txt', 'r') as settings:
+    tmp = [float(num) for num in settings.read().split("\n")]
 
-time_step = Get_Step()
+time_step = 1 / tmp[0]
+volt_step = tmp[1]
+
+volt_arr = ADC_To_Voltage (volt_step)
 time_arr = Time_Division (volt_arr, time_step)
 
 plt.figure(figsize = (16, 10), dpi = 80)
@@ -31,7 +30,7 @@ plt.figure(figsize = (16, 10), dpi = 80)
 plt.title ('Исследование зарядки и  разрядки конденсатора в RC-цепи', loc = 'center', fontsize = 24)
 
 plt.xlabel('t, c', fontsize = 14)
-plt.xlim(0, (time_arr.size) * time_step)
+plt.xlim(0, time_arr.max() + 1)
 plt.xticks(ticks = np.arange(0, time_arr.max(), time_step * 1000), fontsize = 12, rotation = 30, ha = 'center', va = 'top') 
 
 plt.ylabel('U, В', fontsize = 14)
@@ -49,7 +48,6 @@ plt.grid(which='minor',
          linewidth = 0.25,
          linestyle = 'dashed')
 
-
 charge_time = 0
 index = 0
 while volt_arr[index] < volt_arr.max():
@@ -58,12 +56,12 @@ while volt_arr[index] < volt_arr.max():
 
 discharge_time = time_arr.max() - charge_time
 
-plt.text(130, 1.5, 'Время зарядки = %.2f'   %charge_time,    fontsize = 14)
-plt.text(130, 2,   'Время разрядки = %.2f'  %discharge_time, fontsize = 14)
+plt.text(2 * time_arr.max() / 3, volt_arr.max() / 2 + 0.5, 'Время зарядки = %.2f с'   %charge_time,    fontsize = 14)
+plt.text(2 * time_arr.max() / 3, volt_arr.max() / 2,   'Время разрядки = %.2f с'  %discharge_time, fontsize = 14)
 
-plt.scatter(time_arr, volt_arr, s = 1, color = 'red')
+plt.scatter(time_arr, volt_arr, s = 0.01, color = 'red')
 
-plt.plot(time_arr, volt_arr, 'r', linewidth = 1, color = 'red', label = 'Аппроксимирующая кривая', linestyle = '-')
+plt.plot(time_arr, volt_arr, 'r', linewidth = 1, marker = "o", markevery = 200, label = 'Аппроксимирующая кривая', linestyle = '-')
 plt.legend (loc = 'best', fontsize = 14)
 
 plt.savefig ('Try.png')
